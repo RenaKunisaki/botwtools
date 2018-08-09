db47dc000da3de5a607959f4673a41128c405259
RenaKunisaki <hyperhacker@gmail.com>
2018-08-09 17:40:46 -0400

    improve file magic parsing

--- codec/__init__.py
+++ codec/__init__.py
@@ -11,38 +11,43 @@ encoders = {
 # File magic => decoder class
 decoders = {
     b'AAMP': AampDecoder,
-    b'YB\x02\x00': BymlDecoder,
+    b'BY':   BymlDecoder,
     b'SARC': SarcDecoder,
     b'Yaz0': Yaz0Decoder,
     b'Yaz1': Yaz0Decoder,
+    b'YB':   BymlDecoder,
 }
 
-def getMagic(file):
-    """Given a file, read the 4-byte magic ID from current position.
+def findClass(file, classes):
+    """Given a file and a dict of {magic:class}, try to find a class
+    that supports this file.
 
-    This function does not change the seek position.
+    Reads file "magic" identifiers from the current position.
+    Restores the position afterward.
+
+    Returns the class, or throws UnsupportedFileTypeError.
     """
     pos = file.tell()
-    magic = file.read(4)
-    file.seek(pos, 0) # reset position
-    return magic
+    for magic, cls in classes.items():
+        m = file.read(len(magic))
+        file.seek(pos, 0) # restore position
+        if m == magic: return cls
+    m = file.read(4)
+    file.seek(pos, 0) # restore position
+    raise UnsupportedFileTypeError(m)
+
 
 def getEncoderForFile(file):
     """Given a file, return the Encoder class for it.
 
     Raises UnsupportedFileTypeError if no encoder available.
     """
-    magic = getMagic(file)
-    enc = encoders.get(magic, None)
-    if enc is None: raise UnsupportedFileTypeError(magic)
-    return enc
+    return findClass(file, encoders)
+
 
 def getDecoderForFile(file):
     """Given a file, return the Decoder class for it.
 
     Raises UnsupportedFileTypeError if no decoder available.
     """
-    magic = getMagic(file)
-    dec = decoders.get(magic, None)
-    if dec is None: raise UnsupportedFileTypeError(magic)
-    return dec
+    return findClass(file, decoders)
