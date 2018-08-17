33a22373beecd6ab290b2f08f4177d21a3180820
Rena Kunisaki <hyperhacker@gmail.com>
2018-08-17 19:31:51 -0400

    move FMDL COLLADA exporting to own file

---
+++ codec/fres/fres/fmdl/collada.py
@@ -0,0 +1,286 @@
+# This file is part of botwtools.
+#
+# botwtools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# botwtools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with botwtools.  If not, see <https://www.gnu.org/licenses/>.
+
+import logging; log = logging.getLogger(__name__)
+import struct
+import myxml
+
+attr_types = {
+    # attribute name => {
+    #   name: displayed name
+    #   semantic: the access semantic as specified by COLLADA
+    #   params: list of parameter names
+    # }
+    #
+    # [TEX}BINORMAL, CONTINUITY, IMAGE, INPUT, WEIGHT,
+    # INTERPOLATION, INV_BIND_MATRIX, UV, VERTEX, JOINT,
+    # LINEAR_STEPS, NORMAL, OUTPUT, TEXCOORD, POSITION,
+    # MORPH_{TARGET, WEIGHT}, {TEX}TANGENT,
+    # {IN, OUT}_TANGENT
+    #'_i0': {
+    #    'semantic':'COLOR',
+    #    'params': ('R', 'G', 'B', 'A'),
+    #},
+    '_n0': {
+        'name':    "Normal",
+        'semantic':'NORMAL',
+        'params': ('X', 'Y', 'Z'),
+    },
+    '_p0': {
+        'name':    "Position",
+        'semantic':'POSITION',
+        'params': ('X', 'Y', 'Z'),
+    },
+    '_u0': {
+        'name':    "UV Map",
+        'semantic':'UV',
+        'params': ('U', 'V'),
+    },
+    '_u1': {
+        'name':    "Secondary UV Map",
+        'semantic':'UV',
+        'params': ('U', 'V'),
+    },
+    '_w0': {
+        'name':    "Weight",
+        'semantic':'WEIGHT',
+        'params': ('W',),
+    },
+}
+
+class ColladaWriter:
+    """Takes objects found in an FRES and creates
+    a COLLADA file from them.
+    """
+    def __init__(self):
+        self.cameras    = []
+        self.lights     = []
+        self.effects    = []
+        self.materials  = []
+        self.geometries = []
+        self.scenes     = []
+        self.meshes     = []
+        self.vtxs       = []
+
+
+    def addScene(self):
+        scene = myxml.Element('visual_scene',
+            id   = 'scene%d' % len(self.scenes),
+            name = 'untitled',
+        )
+        self.scenes.append(scene)
+        for geom in self.geometries:
+            gid = geom.get('id')
+            scene.Child('node',
+                myxml.Element('instance_geometry', url='#'+gid),
+                id = 'node_' + gid,
+                name = geom.get('name'),
+            )
+
+
+    def addFVTX(self, fvtx, name=None):
+        """Add an FVTX to the file."""
+        # generate IDs
+        gid  = 'geometry%d' % len(self.geometries)
+        vid  = 'vertices%d' % len(self.geometries)
+        if name is None: name = gid
+
+        # create geometry element
+        elem = myxml.Element('geometry', id=gid, name=name)
+        self.geometries.append(elem)
+
+        # create geometry -> mesh -> vertices
+        mesh = elem.Child('mesh')
+        vtxs = mesh.Child('vertices', id=vid)
+        self.meshes.append(mesh)
+        self.vtxs.append(vtxs)
+
+        # add each attribute to the mesh,
+        # and an input for it to the vertices
+        for attr in fvtx.attrs:
+            src, input = self._serializeAttribute(attr, fvtx)
+            if src:   mesh.append(src)
+            if input: vtxs.append(input)
+
+
+    def addFSHP(self, fshp):
+        """Add an FSHP to the file."""
+        # we need to attach the polylist to a mesh,
+        # and it needs to reference a vertices element by ID.
+        try: mesh = self.meshes[-1]
+        except IndexError:
+            log.error("No mesh to attach FSHP to!")
+            return
+
+        try: vtxs = self.vtxs[-1]
+        except IndexError:
+            log.error("No vertex group to attach FSHP to!")
+            return
+
+        for lod in fshp.lods:
+            plist = self._makePlistForLod(lod, vtxs.get('id'))
+            if plist: mesh.append(plist)
+
+
+    def _makePlistForLod(self, lod, vid):
+        """Build plist element for LOD model. Called by addFSHP."""
+        # <lines>, <linestrips>, <polygons>, <polylists>, <triangles>, <trifans> and <tristrips>
+        if lod.prim_fmt in ('line_strip', 'line_loop'):
+            #elem = 'lines'
+            elem = 'triangles' # XXX
+        elif lod.prim_fmt == 'triangles':
+            elem = 'triangles'
+        else:
+            log.error("Unsupported prim fmt %s", lod.prim_fmt)
+            return None
+
+        plist = myxml.Element(elem,
+            myxml.Element('input',
+                offset   = '0',
+                semantic = 'VERTEX',
+                source   = vid,
+            ),
+            count=len(lod.faces))
+
+        sizes = []
+        faces = []
+        for face in lod.faces:
+            sizes.append(len(face))
+            faces += face
+
+        vcount = plist.Child('vcount')
+        vcount.text = ' '.join(map(str, sizes))
+        p = plist.Child('p')
+        p.text = ' '.join(map(str, faces))
+
+        return plist
+
+
+    def _serializeAttribute(self, attr, fvtx):
+        """Given an attribute in an FVTX, create the elements for it."""
+        # first, a source
+        if attr.name in attr_types:
+            name = attr_types[attr.name]['name']
+        else:
+            name = attr.name
+        elem = myxml.Element('source', id=attr.name, name=name)
+
+        # get the buffer and format
+        buf  = fvtx.buffers[attr.buf_idx]
+        fmt  = fvtx.attrFmts.get(attr.format)
+        func = None
+        if type(fmt) is dict: # we have a conversion function
+            func = fmt['func']
+            fmt  = fmt['fmt']
+
+        # get the data from the buffer
+        data = []
+        sz   = struct.calcsize(fmt)
+        for i in range(int(buf.size / sz)):
+            d = struct.unpack_from(fmt, buf.data, i*sz)
+            if func: d = func(d)
+            for item in d: data.append(item)
+
+        # stupid
+        if attr.name == '_p0':
+            data = self._fixBufferForBlender(data)
+
+        # make elements for the data
+        arr = self._makeArrayForAttribute   (attr, data)
+        acc = self._makeAccessorForAttribute(attr, data)
+        if arr: elem.append(arr)
+        if acc: elem.append(acc)
+
+        # create the input element for this source
+        if attr.name in attr_types:
+            input = myxml.Element('input',
+                semantic = attr_types[attr.name]['semantic'],
+                source = '#'+attr.name,
+            )
+        else: # we don't know what to use this attribute for
+            input = None
+
+        return elem, input
+
+
+    def _fixBufferForBlender(self, data):
+        """Take `data` and return a copy with every 4th element removed.
+        This is necessary to remove the W position from vertices
+        because Blender refuses to import a Collada file whose
+        vertices don't have a stride of 3.
+        """
+        res = []
+        for i in range(0, len(data), 4):
+            res += data[i:i+3] # skip every 4th item
+        return res
+
+
+    def _makeArrayForAttribute(self, attr, data):
+        """Make the array element for an attribute's data."""
+        # XXX use attr.format instead of always float
+        arr = myxml.Element('float_array',
+            id = 'array%d' % id(attr),
+            count = len(data),
+        )
+        arr.text = ' '.join(map(str, data))
+        return arr
+
+
+    def _makeAccessorForAttribute(self, attr, data):
+        """Make the accessor/technique elements for an attribute."""
+        # XXX support more types of accessor/technique
+        if attr.name not in attr_types: return None
+        offs = attr.buf_offs #* buf.stride
+        tech = myxml.Element('technique_common')
+        params = attr_types[attr.name]['params']
+        acc  = tech.Child('accessor',
+            count  = len(data)*len(params), # XXX what is this
+            offset = offs,
+            stride = len(params),
+            source = '#array%d' % id(attr),
+        )
+        for p in params:
+            acc.Child('param', name=p, type='float')
+        return tech
+
+
+    def toXML(self):
+        """Generate XML document for this file."""
+        document = myxml.Document('COLLADA',
+            myxml.Element('library_cameras'),
+            myxml.Element('library_lights'),
+            self.makeLibraryEffectsNode(),
+            self.makeLibraryMaterialsNode(),
+            myxml.Element('library_lights'),
+            myxml.Element('library_geometries', *self.geometries),
+            myxml.Element('library_visual_scenes', *self.scenes),
+            xmlns="http://www.collada.org/2005/11/COLLADASchema",
+            version="1.4.1",
+        )
+        root = document.root
+        for scene in self.scenes:
+            root.Child('scene').Child(
+                'instance_visual_scene', url='#'+scene.get('id'))
+        return document
+
+    def makeLibraryEffectsNode(self):
+        """Make the library_effects XML node."""
+        elem = myxml.Element('library_effects')
+        return elem
+
+    def makeLibraryMaterialsNode(self):
+        """Make the library_materials XML node."""
+        elem = myxml.Element('library_materials')
+        return elem
---
+++ codec/fres/fres/fmdl/object.py
@@ -0,0 +1,119 @@
+# This file is part of botwtools.
+#
+# botwtools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# botwtools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with botwtools.  If not, see <https://www.gnu.org/licenses/>.
+
+import logging; log = logging.getLogger(__name__)
+import myxml
+import struct
+from ..fresobject import FresObject
+from codec.base.types import Offset, Offset64, StrOffs, Padding
+from structreader import StructReader, BinaryObject
+from ..fmat import FMAT
+from ..fshp import FSHP
+from ..fskl import FSKL
+from ..fvtx import FVTX
+from .collada import ColladaWriter
+
+
+class FMDL(FresObject):
+    """FMDL object header."""
+    defaultFileExt = 'dae'
+    # offsets in this struct are relative to the beginning of
+    # the FRES file.
+    # I'm assuming they're 64-bit.
+    _magic = b'FMDL'
+    _reader = StructReader(
+        ('4s', 'magic'),
+        ('I',  'size'),
+        ('I',  'size2'),
+        Padding(4),
+        StrOffs('name'),
+        Padding(4),
+        Offset64('str_tab_end'),
+        Offset64('fskl_offset'),
+
+        Offset64('fvtx_offset'),
+        Offset64('fshp_offset'),
+        Offset64('fshp_dict_offset'),
+        Offset64('fmat_offset'),
+        Offset64('fmat_dict_offset'),
+        Offset64('udata_offset'),
+        Offset64('unk60'),
+        Offset64('unk68'), # probably dict for unk60
+
+        ('H',  'fvtx_count'),
+        ('H',  'fshp_count'),
+        ('H',  'fmat_count'),
+        ('H',  'udata_count'),
+        ('H',  'total_vtxs'),
+        Padding(6),
+        size = 0x78,
+    )
+
+    def readFromFRES(self, fres, offset=None, reader=None):
+        """Read the archive from given FRES."""
+        super().readFromFRES(fres, offset, reader)
+
+        log.debug("FMDL name: '%s'", self.name)
+        self.dumpToDebugLog()
+        #self.dumpOffsets()
+
+        log.info("FMDL '%s' contains %d skeletons, %d FVTXs, %d FSHPs, %d FMATs, %d udatas, total %d vertices",
+            self.name,
+            1 if self.fskl_offset > 0 else 0, # can this ever be 0?
+            self.fvtx_count, self.fshp_count, self.fmat_count,
+            self.udata_count, self.total_vtxs)
+
+        # read skeleton
+        self.skeleton = FSKL().readFromFRES(fres, self.fskl_offset)
+
+        # read vertex objects
+        self.fvtxs = []
+        for i in range(self.fvtx_count):
+            vtx = FVTX().readFromFRES(fres,
+                self.fvtx_offset + (i*FVTX._reader.size))
+            self.fvtxs.append(vtx)
+
+        # read shapes
+        self.fshps = []
+        for i in range(self.fshp_count):
+            self.fshps.append(FSHP().readFromFRES(fres,
+                self.fshp_offset + (i*FSHP._reader.size)))
+
+        # read materials
+        self.fmats = []
+        for i in range(self.fmat_count):
+            self.fmats.append(FMAT().readFromFRES(fres,
+                self.fmat_offset + (i*FMAT._reader.size)))
+
+        #self.fshps = [self.fshps[1]] # XXX DEBUG only keep one model
+
+        return self
+
+
+    def validate(self):
+        super().validate()
+        return True
+
+
+    def toData(self):
+        """Export model to COLLADA file."""
+        writer = ColladaWriter()
+
+        for i, fvtx in enumerate(self.fvtxs):
+            writer.addFVTX(fvtx, name=self.fshps[i].name)
+            writer.addFSHP(self.fshps[i])
+
+        writer.addScene()
+        return writer.toXML().tostring(pretty_print=True)
