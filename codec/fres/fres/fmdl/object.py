1414e221748374a2997722f61b42261b5d60fbdc
Rena Kunisaki <hyperhacker@gmail.com>
2018-08-20 20:57:20 -0400

    remove some debug messages

--- codec/fres/fres/fmdl/object.py
+++ codec/fres/fres/fmdl/object.py
@@ -70,7 +70,7 @@ class FMDL(FresObject):
         self.dumpToDebugLog()
         #self.dumpOffsets()
 
-        log.info("FMDL '%s' contains %d skeletons, %d FVTXs, %d FSHPs, %d FMATs, %d udatas, total %d vertices",
+        log.debug("FMDL '%s' contains %d skeletons, %d FVTXs, %d FSHPs, %d FMATs, %d udatas, total %d vertices",
             self.name,
             1 if self.fskl_offset > 0 else 0, # can this ever be 0?
             self.fvtx_count, self.fshp_count, self.fmat_count,
